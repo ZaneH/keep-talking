13ac3c300ab7b447cd555af6ece48f082a066993
Zane Helton <zane99@me.com>
2025-05-05 02:42:45 -0400

    refactor: change name of grpc adapter

--- internal/application/command/player_input_command.go
+++ internal/application/command/player_input_command.go
@@ -6,12 +6,6 @@ import (
 )
 
 type PlayerInputCommand struct {
-	// string player_id = 1;
-	// modules.ModulePosition module_position = 2;
-	// oneof input {
-	//   modules.CutWireInput cut_wire = 3;
-	//   modules.SubmitPasswordInput submit_password = 4;
-	// }
 	SessionId      uuid.UUID
 	ModulePosition valueobject.ModulePosition
 	Input          interface{}
--- internal/infrastructure/grpc/game_service_adapter.go
+++ internal/infrastructure/grpc/game_service_adapter.go
@@ -9,21 +9,21 @@ import (
 	"google.golang.org/grpc"
 )
 
-type GameServer struct {
+type GameServiceAdapter struct {
 	pb.UnimplementedGameServiceServer
 	gameService *services.GameService
 }
 
-func NewGameServiceAdapter(gameService *services.GameService) *GameServer {
-	return &GameServer{gameService: gameService}
+func NewGameServiceAdapter(gameService *services.GameService) *GameServiceAdapter {
+	return &GameServiceAdapter{gameService: gameService}
 }
 
-func (s *GameServer) CreateGame(context.Context, *pb.CreateGameRequest) (*pb.CreateGameResponse, error) {
+func (s *GameServiceAdapter) CreateGame(context.Context, *pb.CreateGameRequest) (*pb.CreateGameResponse, error) {
 	fmt.Println("CreateGame called")
 	return nil, nil
 }
 
-func (s *GameServer) SendInput(context context.Context, i *pb.PlayerInput) (*pb.PlayerInputResult, error) {
+func (s *GameServiceAdapter) SendInput(context context.Context, i *pb.PlayerInput) (*pb.PlayerInputResult, error) {
 	fmt.Println("SendInput called")
 	fmt.Println("Position:", i.GetModulePosition())
 	switch i.GetInput().(type) {
@@ -33,6 +33,8 @@ func (s *GameServer) SendInput(context context.Context, i *pb.PlayerInput) (*pb.
 	case *pb.PlayerInput_SubmitPassword:
 		fmt.Println("SubmitPassword command received")
 		fmt.Println("Password: ", i.GetSubmitPassword().Password)
+	default:
+		fmt.Println("Unknown command type")
 	}
 	return &pb.PlayerInputResult{
 		ModuleId: "module-id",
@@ -40,7 +42,7 @@ func (s *GameServer) SendInput(context context.Context, i *pb.PlayerInput) (*pb.
 	}, nil
 }
 
-func (s *GameServer) StreamGameState(*pb.GameStateRequest, grpc.ServerStreamingServer[pb.GameState]) error {
+func (s *GameServiceAdapter) StreamGameState(*pb.GameStateRequest, grpc.ServerStreamingServer[pb.GameState]) error {
 	fmt.Println("StreamGameState called")
 	return nil
 }
